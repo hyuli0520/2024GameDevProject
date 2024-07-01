211ec9b627ee8e9bec16195c4a1836d1755e50cb
Minhyeok Jeon <minhyeogjeon945@gmail.com>
2024-07-01 17:16:32 +0900

    GetDamage, multiplier

--- Assets/Scripts/HealthTypeComponent.cs
+++ Assets/Scripts/HealthTypeComponent.cs
@@ -7,13 +7,21 @@ public class HealthTypeComponent : HealthComponent, ITypeDamageReceiver
     [SerializeField]
     Type type;
 
+    float weakMultiplier = 1.5f;
+    float strongMultiplier = 0.5f;
+
     public void GetTypeDamage(ITypeDamage damage)
     {
         if (type.strongType == damage.Type)
-            Health -= (damage.Amount * 0.5f);
+            GetDamage(damage, strongMultiplier);
         else if (type.weakType == damage.Type)
-            Health -= (damage.Amount * 1.5f);
+            GetDamage(damage, weakMultiplier);
         else
-            Health -= damage.Amount;
+            GetDamage(damage);
+    }
+
+    private void GetDamage(ITypeDamage damage, float multiplier = 1f)
+    {
+        Health -= damage.Amount * multiplier;
     }
 }
