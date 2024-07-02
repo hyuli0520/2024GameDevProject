8f424c90173c76e36b3a05a0edf223bb7cc9ba75
Minhyeok Jeon <minhyeogjeon945@gmail.com>
2024-07-02 20:13:49 +0900

    공격 딜레이

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -5,5 +5,22 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour, IPlayerAttack
 {
     [SerializeField]
-    private GameObject _bulletPrefab;
+    private GameObject _bulletPrefab; // ÃÑ¾Ë ÇÁ¸®ÆÕ
+    [SerializeField]
+    private Transform _firingPos; // ¹ß»ç À§Ä¡
+
+    [SerializeField]
+    private float _delay;
+    private float _lastShootTime;
+    private float _shootTime = float.MaxValue;
+
+    public void Shoot()
+    {
+        if(_shootTime - _lastShootTime > _delay)
+        {
+            Instantiate(_bulletPrefab, _firingPos.position, transform.rotation);
+            _lastShootTime = Time.time;
+        }
+        _shootTime = Time.time;
+    }
 }
