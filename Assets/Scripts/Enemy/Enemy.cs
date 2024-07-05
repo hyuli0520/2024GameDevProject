b43c8cd53e2e0a4ebd7d178d435af548d2da1a06
Minhyeok Jeon <minhyeogjeon945@gmail.com>
2024-07-05 19:13:43 +0900

    적 공격

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : MonoBehaviour, IDamage
 {
+    public float Amount => _damage;
+
+    [SerializeField]
+    private float _damage = 1;
+
     public float speed;
 
     [SerializeField]
@@ -26,5 +31,13 @@ public class Enemy : MonoBehaviour
         Vector2 nextVec = dirVec.normalized * speed * Time.deltaTime;
         _rigid.MovePosition(_rigid.transform.position + (Vector3)nextVec);
         _rigid.velocity = Vector2.zero;
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<IDamageReceiver>(out var damageReceiver))
+        {
+            damageReceiver.GetDamage(this);
+        }
     }
 }
